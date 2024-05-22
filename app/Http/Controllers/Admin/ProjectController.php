2d4f6f7096f5b6730afe5ed443eeb03a6b9142a3
luigidibella <lykos.omega@gmail.com>
2024-05-23 01:26:09 +0200

    [add] admin.projects.destroy

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -82,8 +82,10 @@ class ProjectController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index')->with('deleted', 'Il progetto' . ' "' . $project->title . '" ' . 'è stato eliminato.');
     }
 }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <h2 class="text text-white">Elenco Progetti</h2>
 
+    @if (session('deleted'))
+
+    <div class="alert alert-success" role="alert">
+        {{ session('deleted')}}
+    </div>
+
+    @endif
+
     <table class="table table-success crud-table">
         <thead>
           <tr>
@@ -21,7 +29,16 @@
               <td>
                 <div class="d-flex">
                     <a href="{{ route('admin.projects.edit', $item->id) }}" class="btn btn-warning me-2"><i class="fa-regular fa-pen-to-square"></i></a>
-                    <a href="{{ route('admin.projects.edit', $item->id) }}" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></a>
+                    <form
+                        class="d-inline"
+                        action="{{ route('admin.projects.destroy', $item->id) }}"
+                        method="POST"
+                        onsubmit="return confirm('Sei sicuro di vole eliminare \'{{ $item->title }}\'?')"
+                    >
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
+                    </form>
                 </div>
               </td>
             </tr>
